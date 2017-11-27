9fc0ec7a2dc21dcdc9eaad711b6eb8b2634eccae
Misha Klein <klein.misha@gmail.com>
2017-11-27 17:00:19 +0100

    working example for code that specifically works with sigmoid

--- SigmoidTest.py
+++ SigmoidTest.py
@@ -1,19 +1,32 @@
 import numpy as np
-from SimulatedAnnealing_MultiProcessing import sim_anneal_fit as simann
+from SimulatedAnnealing import sim_anneal_fit as simann
 import matplotlib.pyplot as plt
 
+
+
+
+
+
+
+
+
 # Create data
-x = np.linspace(0,100,1000)
-y = 10 / ( 1 + np.exp( (40-x) * 20 )  ) + np.random.rand(len(x))
-yerr = np.array([1]*len(x))
+# x = np.linspace(0,100,1000)
+A = 1.0
+C = 0.75
+nseed = 8
+yerr = 0.05
+x = np.arange(1,20)
+y = A / (1 + np.exp(-(x-nseed) * C)) + yerr * np.random.rand(len(x))
 
 # Fit parameters
-start_params = np.array([3,30,13])
-lwr_bnd = np.array([-100,-100,-100])
-uppr_bnd = np.array([100,100,100])
+start_params = np.array([5,35,6])
+lwr_bnd = [0.0, 1.0, 0.0]
+uppr_bnd  = [10.0,20.0, 4.0 ]
+
 
 # Do fit
-fit = simann(x,y,yerr,start_params,lwr_bnd,uppr_bnd)
+fit = simann(x,y,yerr,start_params,lwr_bnd,uppr_bnd,Tfinal=1.0)
 
 # Plot results
 plt.errorbar(x,y,yerr=yerr,fmt='x')
